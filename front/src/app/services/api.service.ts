9224c8f844d5cca6d0d5be29ee656022a3943668
simoninformatico <loossimon@cy-tech.fr>
2025-05-21 16:35:50 +0200

    oue

--- front/src/app/services/api.service.ts
+++ front/src/app/services/api.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+export interface User {
+  id: number;
+  username: string;
+  twitch_channel?: string;
+  youtube_live_chat_id?: string;
+  youtube_video_id?: string;
+  tiktok_username?: string;
+  is_viewer ?: boolean;
+}
 export class ApiService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8000/users/streamers'; // adapte selon ton backend
+
+  constructor(private http: HttpClient) {}
+
+  getStreamers(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl);
+  }
+
 }
